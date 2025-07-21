67e84f42047dc3eccc903fd44bee519c4e9a9f5e
Isaac Phiri <isaacphiri315@gmail.com>
2025-07-21 10:23:23 +0200

    edit: entry point

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,21 @@
-main.py
+# main.py
+# import os
+# import sys
+
+# sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+# print("Current sys.path:", sys.path)
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.api.routes import router as api_router
+
+app = FastAPI()
+app.include_router(api_router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
